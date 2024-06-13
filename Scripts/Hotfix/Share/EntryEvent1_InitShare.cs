e040f0bd34fd9ac0667179d992036526255a0352
tanghai <egametang@gmail.com>
2024-06-14 00:23:27 +0800

    帧同步进入UI界面

---
+++ Scripts/Hotfix/Share/EntryEvent1_InitShare.cs
@@ -0,0 +1,17 @@
+namespace ET
+{
+    [Event(SceneType.LockStep)]
+    public class EntryEvent1_InitShare: AEvent<Scene, EntryEvent1>
+    {
+        protected override async ETTask Run(Scene root, EntryEvent1 args)
+        {
+            root.AddComponent<TimerComponent>();
+            root.AddComponent<CoroutineLockComponent>();
+            root.AddComponent<ObjectWait>();
+            root.AddComponent<MailBoxComponent, int>(MailBoxType.UnOrderedMessage);
+            root.AddComponent<ProcessInnerSender>();
+            
+            await ETTask.CompletedTask;
+        }
+    }
+}
--- Scripts/HotfixView/Client/UI/UILSLogin/UILSLoginEvent.cs
+++ Scripts/HotfixView/Client/UI/UILSLogin/UILSLoginEvent.cs
@@ -8,7 +8,13 @@ namespace ET.Client
     {
         public override async ETTask<UI> OnCreate(UIComponent uiComponent, UILayer uiLayer)
         {
-            string assetsName = $"Assets/Bundles/UI/LockStep/{UIType.UILSLogin}.prefab";
+            string assetsName = $"Packages/cn.etetet.lockstep/Bundles/UI/{UIType.UILSLogin}.prefab";
+
+            if (uiComponent.Scene().GetComponent<ResourcesLoaderComponent>() == null)
+            {
+                Log.Debug($"111111111111111111111111111111111111111");
+            }
+            
             GameObject bundleGameObject = await uiComponent.Scene().GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
             GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject, uiComponent.UIGlobalComponent.GetLayer((int)uiLayer));
             UI ui = uiComponent.AddChild<UI, string, GameObject>(UIType.UILSLogin, gameObject);
