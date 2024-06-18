874061356a0029b0b5e3926bf6923b7f76f7e94a
tanghai <egametang@gmail.com>
2024-06-18 20:12:05 +0800

    去掉不用的log

--- Scripts/HotfixView/Client/UI/UILSLogin/UILSLoginEvent.cs
+++ Scripts/HotfixView/Client/UI/UILSLogin/UILSLoginEvent.cs
@@ -9,12 +9,6 @@ namespace ET.Client
         public override async ETTask<UI> OnCreate(UIComponent uiComponent, UILayer uiLayer)
         {
             string assetsName = $"Packages/cn.etetet.lockstep/Bundles/UI/{UIType.UILSLogin}.prefab";
-
-            if (uiComponent.Scene().GetComponent<ResourcesLoaderComponent>() == null)
-            {
-                Log.Debug($"111111111111111111111111111111111111111");
-            }
-            
             GameObject bundleGameObject = await uiComponent.Scene().GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
             GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject, uiComponent.UIGlobalComponent.GetLayer((int)uiLayer));
             UI ui = uiComponent.AddChild<UI, string, GameObject>(UIType.UILSLogin, gameObject);
